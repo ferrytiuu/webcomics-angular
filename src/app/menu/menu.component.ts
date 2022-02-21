338c5e4be3d341898590b11073dcd6646ca4755f
dafepe2021asix2 <15585360.clot@fje.edu>
2022-02-22 00:04:43 +0100

    quitar clic en menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import {WebcomicServei} from "../dadesServei/servei_Webcomic";
 @Component({
   selector: 'aplicacio',
+  styleUrls: ['./menu.component.css'],
   template: `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">{{titol}}</a>
+    <a [ngClass]="menuEstado.estado==true ? ' ' : 'isDisabled' " class="navbar-brand" href="#">{{titol}}</a>
     <button *ngIf="menuEstado.estado==true" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
